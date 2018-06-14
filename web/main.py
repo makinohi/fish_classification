4a7641590c991a9aba0bd4e9d5b5553ed43732af
yutaroha-ml <yutaroha.ml@gmail.com>
2018-06-14 21:16:59 +0900

    [add]WebAPI追加(ダミーデータ返却するように)

--- web/main.py
+++ web/main.py
@@ -7,5 +7,21 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/api/fish-classification', methods=['POST'])
+def classification():
+    #ダミーデータ返却
+    result = [
+        {   'name' : 'haze',
+            'confidence' : '0.98828125'
+        },
+        {   'name' : 'kasago',
+            'confidence' : '0.0012414'
+        },
+        {   'name' : 'kisu',
+            'confidence' : '0.0000414'
+        }
+    ]
+    return jsonify({'result': result})
+
 if __name__ == '__main__':
     app.run()
