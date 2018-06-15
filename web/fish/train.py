cc78a53d7b8005f158e3dd862546e6c895487496
yutaroha-ml <yutaroha.ml@gmail.com>
2018-06-15 23:26:43 +0900

    [add]CNN処理追加。

--- web/fish/train.py
+++ web/fish/train.py
@@ -1,53 +1,107 @@
-import numpy
-from tensorflow.python.keras.datasets import mnist
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense
-from tensorflow.python.keras.callbacks import TensorBoard
-from tensorflow.python.keras.utils import to_categorical
+import os
+import keras
+from keras.utils import np_utils
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.preprocessing.image import array_to_img, img_to_array, list_pictures, load_img
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+epochs = 20
+batch_size = 32
+img_size = (128,128)
+category_size = 3
+model_file_path = './model/model_weight.h5'
 
 def load_data():
-    #とりあえず仮でMNISTのデータ返却
-    (x_train,y_train), (x_test,y_test) = mnist.load_data()
-    return (x_train,y_train), (x_test,y_test)
+    x_data = []
+    y_data = []
 
-def execute_train(train, test):
-    x_train = train[0]
-    y_train = train[1]
+    # appleの画像
+    for picture in list_pictures('./dataset/apple/'):
+        img = img_to_array(load_img(picture, target_size=img_size))
+        x_data.append(img)
+        y_data.append(0)
 
-    x_train = x_train.reshape(60000,784)
-    x_train = x_train/255
+    # bananaの画像
+    for picture in list_pictures('./dataset/banana/'):
+        img = img_to_array(load_img(picture, target_size=img_size))
+        x_data.append(img)
+        y_data.append(1)
 
-    y_train = to_categorical(y_train,10)
+    # orangeの画像
+    for picture in list_pictures('./dataset/orange/'):
+        img = img_to_array(load_img(picture, target_size=img_size))
+        x_data.append(img)
+        y_data.append(2)
 
+    # arrayに変換
+    x_data = np.asarray(x_data)
+    y_data = np.asarray(y_data)
+
+    # 画素値を0から1の範囲に変換
+    x_data = x_data.astype('float32')
+    x_data = x_data / 255.0
+
+    # クラスの形式を変換
+    y_data = np_utils.to_categorical(y_data, category_size)
+
+    # 学習用データとテストデータの振り分け
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.33, random_state=123)
+
+    return x_train, x_test, y_train, y_test
+
+def create_model():
     model = Sequential()
-    model.add(
-            Dense(
-                units = 64,
-                input_shape=(784,),
-                activation='relu'
-            )
-    )
-    model.add(
-            Dense(
-                units = 10,
-                activation='softmax'
-            )
-    )
-    model.compile(
-        optimizer='adam',
-        loss = 'categorical_crossentropy',
-        metrics=['accuracy']
-    )
-    tsb=TensorBoard(log_dir='./logs')
-    history_adam=model.fit(
-        x_train,
-        y_train,
-        batch_size=32,
-        epochs=20,
-        validation_split=0.2,
-        callbacks=[tsb]
-    )
-    
+
+    model.add(Conv2D(32, (3, 3), padding='same',
+                    input_shape = (128,128,3)))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(category_size))
+    model.add(Activation('softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                optimizer='rmsprop',
+                metrics=['accuracy']) 
+    return model
+
+def load_weight(model, file_path):
+    if not os.path.exists(file_path):
+        return 
+    model.load_weights(file_path)
+
+def fit(model, x_train, x_test, y_train, y_test):
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data = (x_test, y_test))
+    model.save(model_file_path)
+
 if __name__ == '__main__':
-    train, test = load_data()
-    execute_train(train,test)
+
+    #教師用データ、テスト用データ読込
+    x_train, x_test, y_train, y_test = load_data()
+
+    #モデル生成
+    model = create_model()
+
+    #重み読込
+    load_weight(model,model_file_path)
+
+    #学習
+    fit(model, x_train, x_test, y_train, y_test)
